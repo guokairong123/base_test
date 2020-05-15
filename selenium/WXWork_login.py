8054df2548dafba86715ec9dde56d5c9b47c4e09
99292 <992926186@qq.com>
2020-05-15 23:58:11 +0800

    代码提交

--- selenium/WXWork_login.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-
-class WXWrokLogin():
-    def setup(self):
-        self.driver = webdriver.Chrome()
-    def teardown(self):
-        pass
-    def WXWork_login(self):
